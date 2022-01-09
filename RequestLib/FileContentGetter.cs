3021059fd7853a31677a51eca5a391619d5876cf
Вера Пирог <vera.planet3@gmail.com>
2022-01-09 23:28:17 +0200

    Requestslib

---
+++ RequestLib/FileContentGetter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestLib
+{
+	class FileContentGetter : Requester
+	{
+		public FileContentGetter() : base()
+		{
+		}
+
+		private async Task<string> GetFileContent(Path path)
+		{
+			var stringTask = client.GetStringAsync("https://raw.githubusercontent.com/" + path.full_repo_name + "/master/" + path.path_to_file);
+
+			return await stringTask;
+		}
+	}
+}
